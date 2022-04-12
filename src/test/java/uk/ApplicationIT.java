05dda9f275ee245812d5dc6f43e1eb65021606f2
paul campbell <>
2022-04-12 16:36:52 +0100

    initial version

---
+++ src/test/java/uk/ApplicationIT.java
@@ -0,0 +1,12 @@
+package uk;
+
+import org.junit.jupiter.api.Test;
+
+public class ApplicationIT {
+
+    @Test
+    public void integrationTest() {
+        String[] args = {};
+        Application.main(args);
+    }
+}
---
+++ src/test/java/uk/ApplicationTest.java
@@ -0,0 +1,12 @@
+package uk;
+
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest {
+
+    @Test
+    public void unitTest() {
+        String[] args = {};
+        Application.main(args);
+    }
+}
